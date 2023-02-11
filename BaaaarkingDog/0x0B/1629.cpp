e6aafa973d65fdaf302258073760eccda9846823
Daeul <pde9810@gmail.com>
2023-02-11 21:47:38 +0900

    Solved

--- BaaaarkingDog/0x0B/1629.cpp
+++ BaaaarkingDog/0x0B/1629.cpp
@@ -5,7 +5,6 @@ using ll = long long;
 ll a, b, c;
 
 ll my_mul(ll x, ll y) {
-    if(y == 0) return 1;
     if(y == 1) return x % c;
 
     else if(y % 2 == 0) return (my_mul(x, y/2) % c) * (my_mul(x, y/2) % c) % c;
