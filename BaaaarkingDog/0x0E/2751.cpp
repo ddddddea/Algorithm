3d154432b2214a7b640842997edb0d2f426b8693
Daeul <pde9810@gmail.com>
2023-02-14 00:57:03 +0900

    Solved

---
+++ BaaaarkingDog/0x0E/2751.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int ar[1000010];
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int N;
+    cin >> N;
+    for(int i = 0; i < N; i++) cin >> ar[i];
+    sort(ar, ar+N);
+    for(int i = 0; i < N; i++) cout << ar[i] << '\n';
+}
--- BaaaarkingDog/0x15/13414.cpp
+++ BaaaarkingDog/0x15/13414.cpp
@@ -1,8 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
+
 unordered_map<string, int> um;
-map<int, string> m;
-string sl[500010];
+list<string> std_l;
+
 
 int main() {
     ios_base::sync_with_stdio(false);
@@ -10,11 +11,27 @@ int main() {
     int K, L;
     cin >> K >> L;
     for(int i = 0; i < L; i++) {
-         string name;
-         cin >> name;
-        //  if()
-       
+        string std_id;
+        cin >> std_id;
+        std_l.push_back(std_id); // Add std_list
+        if(um.find(std_id) == um.end()) um[std_id] = 1; // Student who first Click
+        else {  
+            um[std_id] += 1;
+        }
     }
 
-   
+    for(auto i = std_l.begin(); i != std_l.end();) {
+        if(um[(*i)] > 1) {
+            um[(*i)]--;
+            i = std_l.erase(i);
+        }
+        else i++;
+    }
+    
+    int cnt = 0;
+    for(auto i = std_l.begin(); i != std_l.end(); i++) {
+        cout << (*i) << '\n';
+        cnt++;
+        if(K == cnt) break;
+    }
 }
